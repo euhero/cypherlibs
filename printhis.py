efcc2140d23b838a8cdcae57678124da2ecca9a6
euhero <45124383+euhero@users.noreply.github.com>
2020-10-07 23:20:47 +0800

    preversioning

---
+++ printhis.py
@@ -0,0 +1,43 @@
+import datetime
+from colorama import init, Fore, Back
+import sys
+import os
+
+class Printhis:
+	def __init__(self,folder_name='logs',logging=True):
+		
+		self.logging = logging
+		self.folder_name = folder_name
+		init(autoreset=True)
+
+	def log(self,message,color='default',vanishlog=False):
+
+		if color == 'default':
+			print(f'[{datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")}]\t: {message.ljust(20)}')
+		if color == 'green':
+			print(Fore.GREEN + Back.RESET + '[' + datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + ']\t: ' + message.ljust(20))
+		if color == 'red':
+			print(Fore.RED + '[' + datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + ']\t: ' + message.ljust(20))
+		if color == 'yellow':
+			print(Fore.YELLOW + '[' + datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + ']\t: ' + message.ljust(20))
+
+		if color == 'log':
+			pass
+
+		if color == 'vanish':
+			sys.stdout.write(f'\r[{datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")}]\t: {message}\r')
+			sys.stdout.flush()
+			if vanishlog is False:
+				return True
+
+		if self.logging:
+
+			if not os.path.isdir(self.folder_name):
+				os.mkdir(self.folder_name)
+
+			with open(self.folder_name + '/logs.txt', 'a', encoding="UTF-8") as logs:
+				logs.writelines(f'[{datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")}]\t: {message}\n')
+
+			if color == 'red':
+				with open(self.folder_name + '/errorlogs.txt', 'a', encoding="UTF-8") as logs:
+					logs.writelines(f'[{datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")}]\t: {message}\n')
